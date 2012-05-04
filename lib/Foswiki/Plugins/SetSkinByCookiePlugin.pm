8407ebf8f09f961d5bd16dc66a149ffece830bc2
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SetSkinByCookiePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/SetSkinByCookiePlugin.pm
+++ lib/Foswiki/Plugins/SetSkinByCookiePlugin.pm
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -wI.
 #
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # This program is free software; you can redistribute it and/or
@@ -11,11 +11,10 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
-# 
+#
 # Author: Eugen Mayer
-
 
 package Foswiki::Plugins::SetSkinByCookiePlugin;
 
@@ -23,61 +22,66 @@ use strict;
 
 use Assert;
 use HTTP::Cookies::Find;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
 
 $VERSION = '$Rev: 12445$';
 
 $RELEASE = '0.2';
 
-$SHORTDESCRIPTION = ' ';
+$SHORTDESCRIPTION  = ' ';
 $NO_PREFS_IN_TOPIC = 1;
 
 $pluginName = 'SetSkinByCookiePlugin';
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 2.0 ) {
-        Foswiku::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+        Foswiku::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
-    
-    _checkCookieAndSetSkin($Foswiki::cfg{Plugins}{SetSkinByCookiePlugin}{CookieName},$Foswiki::cfg{Plugins}{SetSkinByCookiePlugin}{Expire});
+
+    _checkCookieAndSetSkin(
+        $Foswiki::cfg{Plugins}{SetSkinByCookiePlugin}{CookieName},
+        $Foswiki::cfg{Plugins}{SetSkinByCookiePlugin}{Expire}
+    );
     return 1;
 }
 
 sub _checkCookieAndSetSkin {
-    my($cookieName, $expire) = @_;
- 	my $query = Foswiki::Func::getCgiQuery();	
-	my $value = "";
-	
-	foreach my $name ($query->cookie()) {
-		if($name eq $cookieName)
-		{
-			$value = $query->cookie('-name'=>$name);
-		}
- 	}
- 	 	
- 	return if($value eq "");
- 	
-	# using > means, you want this skin to  be added to the current skin, but on the left side
-	# so being more important ( left side )
-	if($value =~ /^<(.*)$/) {
-		my $currentSkin = Foswiki::Func::getSkin();
-		
-		$value = "$1, $currentSkin" if($currentSkin ne ""); 
-	}
-	
-	# same as above, but you want to add it as the most unimportant one ( right side )
-	if($value =~ /^>(.*)$/) {
-        my $currentSkin = Foswiki::Func::getSkin();
-        
-        $value = "$currentSkin, $1" if($currentSkin ne ""); 
+    my ( $cookieName, $expire ) = @_;
+    my $query = Foswiki::Func::getCgiQuery();
+    my $value = "";
+
+    foreach my $name ( $query->cookie() ) {
+        if ( $name eq $cookieName ) {
+            $value = $query->cookie( '-name' => $name );
+        }
     }
+
+    return if ( $value eq "" );
+
+# using > means, you want this skin to  be added to the current skin, but on the left side
+# so being more important ( left side )
+    if ( $value =~ /^<(.*)$/ ) {
+        my $currentSkin = Foswiki::Func::getSkin();
+
+        $value = "$1, $currentSkin" if ( $currentSkin ne "" );
+    }
+
+# same as above, but you want to add it as the most unimportant one ( right side )
+    if ( $value =~ /^>(.*)$/ ) {
+        my $currentSkin = Foswiki::Func::getSkin();
+
+        $value = "$currentSkin, $1" if ( $currentSkin ne "" );
+    }
+
     # if neiher < or > is used, the current Skins get overwritten completely
-    
-	Foswiki::Func::setPreferencesValue("SKIN",$value);		
-	return ;
+
+    Foswiki::Func::setPreferencesValue( "SKIN", $value );
+    return;
 }
 1;
